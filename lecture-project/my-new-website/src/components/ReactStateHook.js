f0cba293021f964af00936e26fd582f8c20e4613
Dan Purcell <purcellgroup88@gmail.com>
2023-07-19 16:44:47 -0400

    updoot

--- lecture-project/my-new-website/src/components/ReactStateHook.js
+++ lecture-project/my-new-website/src/components/ReactStateHook.js
@@ -1,14 +1,36 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function ReactStateHook(){
   //! what is a re-render?
   // whenever this component runs again
-
-
   // invoke a useState setter with a new ref in memory
   const [countSlice, setCountSlice] = useState({ count: 0 })
+  const [data, setData] = useState({})
+
+  //! these uE run asynchronously
+  useEffect(() => {
+    // const getStuff = async () => {
+    //   const res = await fetch("http://somewhere.com")
+    //   const data = await res.json()
+    //   setData(data)
+    // }
+    setData({ resultFromApi: "hewooooo!!!" })
+    // getStuff()
+  }, [])
+
+  useEffect(() => {
+    console.log("hi there", data)
+  }, [data])
+
+  useEffect(() => {
+    
+  })
+  console.log("", data)
 
   let localVar = 0;
+
+  //! short circuit
+  if(!Object.values(data).length) return null
 
   return (
     <div>
@@ -30,6 +52,9 @@ export default function ReactStateHook(){
           console.log("localVar: ", localVar)
         }}
       >change countSlice</button>
+
+
+      <h1>Here's my data!!!!: {data.id && data.resultFromApi}</h1>
     </div>
   )
 }
