8ef59e0d042f68ce9078badd9fd1746ff1db7d10
Dan Purcell <purcellgroup88@gmail.com>
2023-08-14 14:07:22 -0400

    w14d1

--- lecture-project/script2.js
+++
@@ -1,5 +0,0 @@
-// lets see if imports work in the browser
-
-export const header = document.createElement("h2")
-header.textContent = "I was imported from script file 2!"
-
---
+++ lecture-project/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  var content = "hello from react!"
+
+
+  content = "howdy we modified this string (not really)"
+
+
+  return (
+    <div className="App">
+      <h1>{content}</h1>
+    </div>
+  );
+}
+
+export default App;
