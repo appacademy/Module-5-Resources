c29d8e87d5898939773100b32c561a121ae048ac
Dan Purcell <purcellgroup88@gmail.com>
2023-09-13 17:26:51 -0400

    enchanced error handling in lecture example

--- react-app/src/components/Form.js
+++ react-app/src/components/Form.js
@@ -1,18 +1,15 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export default function Form() {
   const [somethingNice, setSomethingNice] = useState("");
   const [result, setResult] = useState("");
-
-  useEffect(() => {
-    console.log("result changed", result);
-  }, [result]);
+  const [serverError, setServerError] = useState({ message: null });
 
   return (
     <form
       onSubmit={(e) => {
         e.preventDefault();
-        sendToServer(url, setResult);
+        sendToServer(url, setResult, setServerError);
       }}
     >
       <div>
@@ -25,32 +22,43 @@ export default function Form() {
           onChange={(event) => setSomethingNice(event.target.value)}
         />
       </div>
+      {serverError.message && (
+        <h5 style={{ color: "red" }}>{serverError.message}</h5>
+      )}
       <button type="button">Cancel</button>
       <button>Submit</button>
       <br />
       <br />
-      <h2>result of submit: {result}</h2>
+      {result && (
+        <img
+          style={{ maxWidth: "200px", maxHeight: "200px" }}
+          src={success}
+          alt="success"
+        />
+      )}
     </form>
   );
 }
 
-function sendToServer(url, setter) {
-  fetch("http://httpstat.us/random/200,201,401,403,400,500-504")
+function sendToServer(url, setSuccess, setError) {
+  fetch(url)
     .then((res) => {
-      if(res.status >= 400){
-        throw new Error("something went wrong in the fetch")
-      }
+      // if bad status code, send to catch
+      if (res.status >= 400) throw res;
       return res.text();
     })
     .then((data) => {
-      setter(data);
+      setError({ message: null })
+      setSuccess(data);
     })
-    .catch((error) => {
-      console.log("there was a fetch error, error: ", error);
+    .catch(async (error) => {
+      // do something with error
+      const errorText = await error.text()
+      setError({ message: errorText });
     });
 }
 
 var success =
   "https://gifdb.com/images/high/saitama-funny-eyebrows-up-d2zddr6q5ligpbie.gif";
 
-var url = "http://httpstat.us/random/200,201,500-504";
+var url = "http://httpstat.us/random/200,201,401,403,400,500-504";
